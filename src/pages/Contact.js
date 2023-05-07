ba7c018d0df3921690e6e7a3a72b77bb4f2b2f2e
Sara Öhman <saraohman97@gmail.com>
2023-05-07 20:12:14 +0200

    Added validation to form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,28 +5,49 @@ import { AiFillLinkedin } from "react-icons/ai";
 import { AiFillYoutube } from "react-icons/ai";
 import { addMessage } from '../data/api.js';
 
-
-
 const Contact = () => {
-  const [name, setName] = useState('')
-  const [mobile, setMobile] = useState('')
-  const [subject, setSubject] = useState('')
-  const [body, setBody] = useState('')
-  // const [error, setError] = useState(false)
 
+  const [message, setMessage] = useState({
+    fullName: '',
+    email: ''
+  })
+  const [error, setError] = useState(false)
+  const [success, setSuccess] = useState(false)
 
   const queryClient = useQueryClient()
 
+
   const addMessageMutation = useMutation(addMessage, {
     onSuccess: () => {
-      queryClient.invalidateQueries("messages")
+      queryClient.invalidateQueries('messages')
     }
   })
 
+  const handleChangeInput = (e) => {
+    setMessage({
+      ...message,
+      [e.target.name]: e.target.value
+    })
+  }
+
   const handleSubmit = e => {
-    e.preventDefault()
-    addMessageMutation.mutate({ name, mobile, subject, body })
-    setName(''); setMobile(''); setSubject(''); setBody('')
+    e.preventDefault();
+    console.log(message)
+      if(message.fullName === '' || message.email === '') {
+        setError(true)
+      }
+      else {
+        setError(false)
+        addMessageMutation.mutate({
+          ...message
+        });
+        setMessage({
+          fullName: '',
+          email: ''
+        });
+        setSuccess(true)
+      }
+
   }
 
   return (
@@ -34,21 +55,31 @@ const Contact = () => {
       <form onSubmit={handleSubmit} className='contact-form'>
         <h1 className='contact-title'>Kontakta oss</h1>
         <div className='input-group'>
-          <label htmlFor="name" className='label'>Fullständigt namn</label> 
+          <label htmlFor="name" className='label'>Fullständigt namn</label>
           <input
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            name='fullName'
+            value={message['fullName']}
+            onChange={handleChangeInput}
             type="text"
             className='input-field'
           />
         </div>
 
         <div className='input-group'>
-          <label htmlFor="text" className='label'>Mobil eller email</label>
-          <input value={mobile} onChange={(e) => setMobile(e.target.value)} type="text" className='input-field' />
+          <label htmlFor="text" className='label'>Email</label>
+          <input
+            name='email'
+            value={message['email']}
+            onChange={handleChangeInput}
+            type="text"
+            className='input-field'
+            />
         </div>
 
-        <div className='input-group'>
+        {error && <p style={{ color: 'red', marginBottom: '1rem' }}>Fälten kan inte vara tomma. </p>}
+        {success && <p style={{ color: 'green', marginBottom: '1rem' }}>Ditt meddelande har skickats. </p>}
+
+        {/* <div className='input-group'>
           <label htmlFor="subject" className='label'>Ärende</label> 
           <input value={subject} onChange={(e) => setSubject(e.target.value)} type="subject" className='input-field' />
         </div>
@@ -56,7 +87,7 @@ const Contact = () => {
         <div className='input-group'>
           <label htmlFor="body" className='label'>Meddelande</label> 
           <textarea value={body} onChange={(e) => setBody(e.target.value)} name="body" cols="30" rows="10" className='input-field' />
-        </div>
+        </div> */}
         <button type='submit' className='btn btn-gray'>Skicka meddelande</button>
       </form>
 
